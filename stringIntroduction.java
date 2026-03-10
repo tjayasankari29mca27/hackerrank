f11911dd238bd1438018a7d2aa4ef5f451a9362a
tjayasankari29mca27 <tjayasankari29_mca27@mepcoeng.ac.in>
2026-03-10 10:28:22 +0530

    Create stringIntroduction.java

---
+++ stringIntroduction.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        String B=sc.next();
+        /* Enter your code here. Print output to STDOUT. */
+        sc.close();
+        
+        // 1. Sum of lengths
+        System.out.println(A.length() + B.length());
+        
+        // 2. Lexicographical comparison
+        // Returns > 0 if A is "greater" than B (A comes after B in dictionary)
+        if (A.compareTo(B) > 0) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+        
+        // 3. Capitalize first letter and print
+        String capA = A.substring(0, 1).toUpperCase() + A.substring(1);
+        String capB = B.substring(0, 1).toUpperCase() + B.substring(1);
+        System.out.println(capA + " " + capB);
+    }
+}
+
+
+
